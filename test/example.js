e4cbd630fade7a5e2806735834cd3f03c25717dc
Milko Škofič <milko@skofic.io>
2022-09-03 21:04:04 +0200

    Working on tests: need to plan structure, added just test templates.

--- test/example.js
+++ test/example.js
@@ -1,9 +1,30 @@
 /*global describe, it */
-'use strict';
-const expect = require('chai').expect;
+'use strict'
 
+//
+// Libraries.
+//
+const expect = require('chai').expect
+
+//
+// Functions.
+//
+const dictionary = require("../utils/dictionary")
+
+/**
+ * Test test.
+ */
 describe('science', function () {
-  it('works', function () {
-    expect(true).not.to.equal(false);
-  });
-});
+    it('works', function () {
+        expect(true).not.to.equal(false)
+    })
+})
+
+describe('Functions', function () {
+  it('one', function () {
+    expect(true).not.to.equal(false)
+  })
+  it('two', function () {
+    expect(true).not.to.equal(false)
+  })
+})
