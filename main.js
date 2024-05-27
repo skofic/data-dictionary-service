19e01ae7a351b378c09a7603b56eabf0e263e547
Milko Škofič <milko@skofic.io>
2024-05-27 12:58:18 +0200

    Removed commented routes in main.js. Atarting to convert all path parameters to path query parameters.

--- main.js
+++ main.js
@@ -9,19 +9,13 @@ const Session = require('./utils/sessions')
 //
 // Routes.
 //
-// module.context.use('/auth', require('./routes/auth'), 'auth')
 module.context.use('/terms', require('./routes/terms'), 'terms')
-// module.context.use('old-terms', require('./routes/old_terms'), 'old-terms')
 module.context.use('/graph', require('./routes/graphs'), 'graphs')
 module.context.use('/enum', require('./routes/enumerations'), 'enum')
 module.context.use('/struct', require('./routes/structures'), 'struct')
 module.context.use('/link', require('./routes/links'), 'link')
 module.context.use('/descr', require('./routes/descriptors'), 'descr')
 module.context.use('/check', require('./routes/validation'), 'check')
-// module.context.use('/util', require('./routes/utils'), 'utils')
-
-// module.context.use('/schemas', require('./routes/schemas'), 'schemas')
-// module.context.use('/topos', require('./routes/topos'), 'topos')
 
 //
 // Sessions.
