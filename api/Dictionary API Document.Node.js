7ae159d0cc538717e31b082bb47ed270e8d35285
Milko Škofič <milko@skofic.io>
2024-07-20 16:32:22 +0200

    Updated service documents with new service.

--- api/Dictionary API Document.Node.js
+++ api/Dictionary API Document.Node.js
@@ -1131,7 +1131,58 @@
     const httpOptions = {
         hostname: 'localhost',
         port: '8529',
-        path: '/_db/EUFGIS/dict/enum/gid/path?path=iso_639_1&code=iso_639_3_eng',
+        path: '/_db/EUFGIS/dict/enum/gid/path?path=iso_639_1&code=iso_639_1_en',
+        method: 'GET',
+        headers: {"Cookie":"FOXXSID=null; FOXXSID.sig=659130347e9eb043443995f5b9aef83b6238df078ef297696fdd33b56bd7b0cb"}
+    };
+    httpOptions.headers['User-Agent'] = 'node ' + process.version;
+ 
+    // Paw Store Cookies option is not supported
+
+    const request = httpTransport.request(httpOptions, (res) => {
+        let responseBufs = [];
+        let responseStr = '';
+        
+        res.on('data', (chunk) => {
+            if (Buffer.isBuffer(chunk)) {
+                responseBufs.push(chunk);
+            }
+            else {
+                responseStr = responseStr + chunk;            
+            }
+        }).on('end', () => {
+            responseStr = responseBufs.length > 0 ? 
+                Buffer.concat(responseBufs).toString(responseEncoding) : responseStr;
+            
+            callback(null, res.statusCode, res.headers, responseStr);
+        });
+        
+    })
+    .setTimeout(0)
+    .on('error', (error) => {
+        callback(error);
+    });
+    request.write("")
+    request.end();
+    
+
+})((error, statusCode, headers, body) => {
+    console.log('ERROR:', error); 
+    console.log('STATUS:', statusCode);
+    console.log('HEADERS:', JSON.stringify(headers));
+    console.log('BODY:', body);
+});
+
+// request Check enumeration element by code, field and path 
+(function(callback) {
+    'use strict';
+        
+    const httpTransport = require('http');
+    const responseEncoding = 'utf8';
+    const httpOptions = {
+        hostname: 'localhost',
+        port: '8529',
+        path: '/_db/EUFGIS/dict/enum/check/field?field=_aid&code=en&type=iso_639_1',
         method: 'GET',
         headers: {"Cookie":"FOXXSID=null; FOXXSID.sig=659130347e9eb043443995f5b9aef83b6238df078ef297696fdd33b56bd7b0cb"}
     };
