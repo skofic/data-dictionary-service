9b5208a787b2a2fbcce5f17a86c1e5525d51d2a9
Milko Škofič <milko@skofic.io>
2024-09-20 16:27:50 +0200

    Changed order.
    Put links after graphs.

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ const Session = require('./utils/sessions')
 //
 module.context.use('/terms', require('./routes/terms'), 'terms')
 module.context.use('/graph', require('./routes/graphs'), 'graphs')
+module.context.use('/link', require('./routes/links'), 'link')
 module.context.use('/enum', require('./routes/enumerations'), 'enum')
 // module.context.use('/struct', require('./routes/structures'), 'struct')
-module.context.use('/link', require('./routes/links'), 'link')
 module.context.use('/descr', require('./routes/descriptors'), 'descr')
 module.context.use('/check', require('./routes/validation'), 'check')
 
