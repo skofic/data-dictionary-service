8a0e2e0d7ffdf2ed5120d49d4fab1833c96db982
Milko Škofič <milko@skofic.io>
2024-09-27 14:20:42 +0200

    Removed enumeration services.
    Now graph services receive the predicate as a parameter.

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ module.context.use('/terms', require('./routes/terms'), 'terms')
 module.context.use('/graph', require('./routes/graphs'), 'graphs')
 module.context.use('/edge', require('./routes/edges'), 'edges')
 module.context.use('/link', require('./routes/links'), 'link')
-module.context.use('/enum', require('./routes/enumerations'), 'enum')
 module.context.use('/descr', require('./routes/descriptors'), 'descr')
 module.context.use('/check', require('./routes/validation'), 'check')
 
